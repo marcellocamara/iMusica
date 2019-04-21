5bc8e7e5420f7d0582a86ce4ec39c9512a64e686
Marcello Câmara <marcellocamara@id.uff.br>
2019-04-21 18:50:26 -0300

    Fix: Unit test
    Real timestamps of mocked API

--- app/src/test/java/dev/marcello/imusica/util/TimestampUtilTest.java
+++ app/src/test/java/dev/marcello/imusica/util/TimestampUtilTest.java
@@ -14,11 +14,11 @@ import static org.junit.Assert.*;
 public class TimestampUtilTest {
 
     private int[] created_utc = {
-            1497903395, 1555763104, 1555777229, 1555735329, 1555773269,
-            1555705461, 1555767605, 1555740306, 1555764899, 1555665621,
-            1555746884, 1555686880, 1555725059, 1555679786, 1555702115,
-            1555698977, 1555705538, 1555703133, 1555701339, 1555603930,
-            1555657694, 1555672619, 1555679956, 1555676786, 1555670386, 1555661100
+            1497903395, 1555705461, 1555725059, 1555665621, 1555686880,
+            1555702115, 1555679786, 1555695502, 1555705538, 1555703133,
+            1555698977, 1555603930, 1555657694, 1555672619, 1555679956,
+            1555676786, 1555670386, 1555661100, 1555629139, 1555657774,
+            1555636168, 1555641702, 1555640881, 1555606250, 1555644464, 1555621697
     };
     private String[] date;
 
@@ -32,32 +32,33 @@ public class TimestampUtilTest {
 
     @Test
     public void convertTime() {
+        //All those timestamp have been converted on the website https://www.epochconverter.com/
         assertThat(date[0], is("19/06/2017"));
-        assertThat(date[1], is("20/04/2019"));
+        assertThat(date[1], is("19/04/2019"));
         assertThat(date[2], is("20/04/2019"));
-        assertThat(date[3], is("20/04/2019"));
-        assertThat(date[4], is("20/04/2019"));
+        assertThat(date[3], is("19/04/2019"));
+        assertThat(date[4], is("19/04/2019"));
         assertThat(date[5], is("19/04/2019"));
-        assertThat(date[6], is("20/04/2019"));
-        assertThat(date[7], is("20/04/2019"));
-        assertThat(date[8], is("20/04/2019"));
+        assertThat(date[6], is("19/04/2019"));
+        assertThat(date[7], is("19/04/2019"));
+        assertThat(date[8], is("19/04/2019"));
         assertThat(date[9], is("19/04/2019"));
-        assertThat(date[10], is("20/04/2019"));
-        assertThat(date[11], is("19/04/2019"));
-        assertThat(date[12], is("20/04/2019"));
+        assertThat(date[10], is("19/04/2019"));
+        assertThat(date[11], is("18/04/2019"));
+        assertThat(date[12], is("19/04/2019"));
         assertThat(date[13], is("19/04/2019"));
         assertThat(date[14], is("19/04/2019"));
         assertThat(date[15], is("19/04/2019"));
         assertThat(date[16], is("19/04/2019"));
         assertThat(date[17], is("19/04/2019"));
-        assertThat(date[18], is("19/04/2019"));
-        assertThat(date[19], is("18/04/2019"));
+        assertThat(date[18], is("18/04/2019"));
+        assertThat(date[19], is("19/04/2019"));
         assertThat(date[20], is("19/04/2019"));
         assertThat(date[21], is("19/04/2019"));
         assertThat(date[22], is("19/04/2019"));
-        assertThat(date[23], is("19/04/2019"));
+        assertThat(date[23], is("18/04/2019"));
         assertThat(date[24], is("19/04/2019"));
-        assertThat(date[25], is("19/04/2019"));
+        assertThat(date[25], is("18/04/2019"));
     }
 
 }
